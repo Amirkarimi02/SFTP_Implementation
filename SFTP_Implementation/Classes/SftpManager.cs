bb6df66cd549c9e1d7ca24c21bd1102f778775f3
Amir <Amir@WIN11LEGION>
2023-07-14 18:19:39 +0330

    Add project files.

---
+++ SFTP_Implementation/Classes/SftpManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Renci.SshNet;
+using System.Threading.Tasks;
+
+namespace SFTP_Implementation.Classes
+{
+    public class SftpManager : IDisposable
+    {
+        private readonly SftpClient _sftpClient;
+
+        public SftpManager(string host, int port, string username, string password)
+        {
+            _sftpClient = new SftpClient(host, port, username, password);
+        }
+
+        public void Connect()
+        {
+            _sftpClient.Connect();
+        }
+
+        public void Disconnect()
+        {
+            _sftpClient.Disconnect();
+        }
+
+        public void UploadFile(string localFilePath, string remoteDirectory)
+        {
+            var fileName = Path.GetFileName(localFilePath);
+            var remoteFilePath = remoteDirectory + "/" + fileName;
+
+            using (var fileStream = File.OpenRead(localFilePath))
+            {
+                _sftpClient.UploadFile(fileStream, remoteFilePath);
+            }
+        }
+
+        public void DownloadFile(string remoteFilePath, string localDirectory)
+        {
+            var fileName = Path.GetFileName(remoteFilePath);
+            var localFilePath = Path.Combine(localDirectory, fileName);
+
+            using (var fileStream = File.OpenWrite(localFilePath))
+            {
+                _sftpClient.DownloadFile(remoteFilePath, fileStream);
+            }
+        }
+
+        public void DeleteFile(string remoteFilePath)
+        {
+            _sftpClient.DeleteFile(remoteFilePath);
+        }
+
+        public void RenameFile(string currentRemoteFilePath, string newRemoteFilePath)
+        {
+            _sftpClient.RenameFile(currentRemoteFilePath, newRemoteFilePath);
+        }
+
+        public void CreateDirectory(string remoteDirectory)
+        {
+            _sftpClient.CreateDirectory(remoteDirectory);
+        }
+
+        public void DeleteDirectory(string remoteDirectory)
+        {
+            _sftpClient.DeleteDirectory(remoteDirectory);
+        }
+
+        public void Dispose()
+        {
+            _sftpClient.Dispose();
+        }
+    }
+}
---
+++ SFTP_Implementation/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using SFTP_Implementation.Classes;
+
+namespace SFTPConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            var host = "your_sftp_hostname";
+            var port = 22;
+            var username = "your_username";
+            var password = "your_password";
+            var localFilePath = "path_to_local_file.txt";
+            var localDirectory = "path_to_local_directory";
+            var remoteDirectory = "/remote/directory";
+            var remoteFilePath = "/remote/file.txt";
+
+            using (var sftpManager = new SftpManager(host, port, username, password))
+            {
+                try
+                {
+                    sftpManager.Connect();
+
+                    // Upload a local file to the SFTP server
+                    sftpManager.UploadFile(localFilePath, remoteDirectory);
+                    Console.WriteLine("File uploaded successfully.");
+
+                    // Download a file from the SFTP server to a local directory
+                    sftpManager.DownloadFile(remoteFilePath, localDirectory);
+                    Console.WriteLine("File downloaded successfully.");
+
+                    // Delete a file from the SFTP server
+                    sftpManager.DeleteFile(remoteFilePath);
+                    Console.WriteLine("File deleted successfully.");
+
+                    // Rename a file on the SFTP server
+                    var newRemoteFilePath = "/remote/new_file.txt";
+                    sftpManager.RenameFile(remoteFilePath, newRemoteFilePath);
+                    Console.WriteLine("File renamed successfully.");
+
+                    // Create a directory on the SFTP server
+                    var newRemoteDirectory = "/remote/new_directory";
+                    sftpManager.CreateDirectory(newRemoteDirectory);
+                    Console.WriteLine("Directory created successfully.");
+
+                    // Delete a directory from the SFTP server
+                    sftpManager.DeleteDirectory(newRemoteDirectory);
+                    Console.WriteLine("Directory deleted successfully.");
+
+                    sftpManager.Disconnect();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An error occurred: " + ex.Message);
+                }
+            }
+        }
+    }
+}
